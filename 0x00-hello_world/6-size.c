e1c506e55718dbf09794eb3873091f2bbfff1829
Maatla-creator <maatlamog@gmail.com>
2021-06-17 13:09:25 -0700

    Print sizes of types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,6 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
- * Print size of types
  */
 
 int main(void)
@@ -14,10 +13,10 @@ long long int c_longlong;
 char d_char;
 float e_float;
 
-printf("Size of a char: %lu byte(s)\n", sizeof(a_int));
-printf("Size of an int: %lu byte(s)\n", sizeof(b_long));
-printf("Size of a long int: %lu byte(s)\n", sizeof(c_longlong));
- printf("Size of a long long int: %lu byte(s)\n", sizeof(d_char));
+printf("Size of a char: %lu byte(s)\n", sizeof(d_char));
+printf("Size of an int: %lu byte(s)\n", sizeof(a_int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(b_long));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(c_longlong));
 printf("Size of a float: %lu byte(s)\n", sizeof(e_float));
 return (0);
 }
