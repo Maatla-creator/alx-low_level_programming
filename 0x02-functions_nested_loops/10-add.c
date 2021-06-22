191212843673f225f4b9651b203fcd2efb7973f2
Maatla-creator <maatlamog@gmail.com>
2021-06-22 14:47:26 -0700

    Add integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * add - adds two integers and returns the result.
+ *
+ * @a: parameter for the add function
+ * @b: second parameter of the add function
+ * Return: sum of a and b
+*/
+
+int add(int a, int b)
+{
+int res;
+res = a + b;
+return (res);
+}
+
