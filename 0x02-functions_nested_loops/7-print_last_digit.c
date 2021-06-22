ce706d956eca8ae501c2c9026ea53a0fb20a1529
Maatla-creator <maatlamog@gmail.com>
2021-06-22 13:50:21 -0700

    Print last digit of number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_last_digit - prints the last digit of a number.
+ *
+ * @n: parameter for the _abs function
+ * Return: n, the value of the last digit
+*/
+int print_last_digit(int n)
+{
+	if (n >= 0)
+	{
+		n = n % 10;
+	}
+	else
+	{
+		n = (n % 10) * -1;
+	}
+
+	_putchar(n + '0');
+	return (n);
+}
