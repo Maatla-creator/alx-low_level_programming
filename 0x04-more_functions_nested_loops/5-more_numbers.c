b00459107f5e43004c985c903dae87f2431e6735
Maatla-creator <maatlamog@gmail.com>
2021-06-24 12:39:42 -0700

    add 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * more_numbers - prints 10 times the numbers 0 to 14 followed by a new line
+ * more_numbers - to print 10x the numbers 0-14, followed by newline
  *
  * Return : void
  */
