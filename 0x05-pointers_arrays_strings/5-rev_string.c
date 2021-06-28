494aa5b8ac686fb88d5f2f28d576288c2243b30b
Maatla-creator <maatlamog@gmail.com>
2021-06-28 13:51:01 -0700

    add updates

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * rev_string - reverses a string
+ *
+ * @s: parameter for the rev_string function
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+
+	int i, n, m;
+	char temp;
+
+	n = 0;
+	while (*(s + n) != '\0')
+	{
+		n++;
+	}
+
+	for (i = 0; i < (n - 1); i++)
+	{
+		for (m = i + 1; m > 0; m--)
+		{
+			temp = *(s + m);
+			*(s + m) = *(s + (m - 1));
+			*(s + (m - 1)) = temp;
+		}
+	}
+}
