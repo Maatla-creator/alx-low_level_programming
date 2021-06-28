e5f801f8eb63489788ad6beb56b0ba0de22f8dfe
Maatla-creator <maatlamog@gmail.com>
2021-06-28 12:30:41 -0700

    first project commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swaps the values of two integers.
+ * @a: first parameter in swap_int.
+ * @b: second parameter in swap_int.
+ * Return: void.
+ */
+void swap_int(int *a, int *b)
+{
+int i;
+int j;
+a = &i;
+b = &j;
+*a = j;
+*b = i;
+}
