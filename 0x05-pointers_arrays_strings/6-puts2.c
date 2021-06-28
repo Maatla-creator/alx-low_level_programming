9e94a61eaac780ef8e9bc1da798c8eda6bd54671
Maatla-creator <maatlamog@gmail.com>
2021-06-28 13:56:20 -0700

    update 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * puts2 -prints every other character of a string,
+ * starting with the first character, followed by a new line.
+ *
+ * @str: parameter for the puts2 function
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*(str + i) != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
