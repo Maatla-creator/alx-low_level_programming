3e9fe6d3b13fcb921a309ad4eb713cac1a38ce6c
Maatla-creator <maatlamog@gmail.com>
2021-07-01 06:08:57 -0700

    capitalizes all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * cap_string - capitalizes all words of a string
+ *
+ * @str: string pointer paramter
+ *
+ * Return: pointer to the modifed string str
+ *
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		if (*(str + i) >= 97 && *(str + i) <= 122
+		    && *(str + (i - 1)) == 32 && i != 0)
+			*(str + i) = 65 + (*(str + i) - 97);
+
+		if ((*(str + i) >= 97 && *(str + i) <= 122
+		    && i == 0) || (*(str + (i - 1)) == 10))
+			*(str + i) = 65 + (*(str + i) - 97);
+
+		i++;
+	}
+
+	return (str);
+}
