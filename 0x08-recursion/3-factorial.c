1e4b120883fd269e21ae11fb096b53dc0ad8481e
Maatla-creator <maatlamog@gmail.com>
2021-07-07 13:38:16 -0700

    Commit files

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,25 @@
+#include "holberton."
+/**
+ * factorial - returns the factorial of the given value
+ *
+ * @n: int parameter in factorial function
+ *
+ * Return: -1 if n is lower than 0, factorial otherwise
+ */
+int factorial(int n)
+{
+
+	if (n  == 0)
+	{
+		return (1);
+	}
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
+
