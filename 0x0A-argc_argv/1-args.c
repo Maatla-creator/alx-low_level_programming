9961a5df9be6c0652e40c945cf6bcbc6db28e7e0
Maatla-creator <maatlamog@gmail.com>
2021-07-12 08:56:36 -0700

    add 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints argument count
+ *@argc: argument count
+ *@argv: array of arguments(strings)
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+
+		printf("%d\n", argc);
+		return (0);
+}
