1d3051a7567ad34b6e9ab0d3d89397ba221e9deb
Maatla-creator <maatlamog@gmail.com>
2021-09-20 12:28:18 -0700

    script to create dynamic library

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 gcc -fPIC -c *.c
 gcc -shared -o liball.so *.o
-LD_LIBRARY_PATH=/root/alx-low_level_programming/0x18-dynamic_libraries:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/root/alx-low_level_programming/0x18-dynamic_libraries:$LD_LIBRARY_PATH
+
